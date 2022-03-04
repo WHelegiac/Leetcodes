acec817e92aea83921fb32b2ed31b71ae1e48236
WHelegiac <WHelegiac1024@163.com>
2022-03-04 14:07:16 +0800

    2022/3/4 到此为止我刷的leetcode 题解

--- answers/46.全排列.cpp
+++ answers/46.全排列.cpp
@@ -2,34 +2,51 @@
  * @Author: WanHao
  * @Date: 2021-11-17 17:52:58
  * @LastEditors: Do not edit
- * @LastEditTime: 2021-11-17 18:32:24
- * @FilePath: \undefinedd:\my_leetcodes_question_answers\46.全排列.cpp
+ * @LastEditTime: 2022-03-01 19:10:24
+ * @FilePath: \my_leetcodes_question_answers\answers\46.全排列.cpp
  * @FileDescription: Lazy~~~
  */
-#include <iostream>
-#include <vector>
-using namespace std;
+// /*
+//  * @Author: WanHao
+//  * @Date: 2021-11-17 17:52:58
+//  * @LastEditors: Do not edit
+//  * @LastEditTime: 2022-03-01 18:36:50
+//  * @FilePath: \my_leetcodes_question_answers\answers\46.全排列.cpp
+//  * @FileDescription: Lazy~~~
+// //  */
 
-class Solution {
-private:
-    void DFS(vector<vector<int>>&res ,vector<int>& nums,int pos){
-        if(pos == nums.size() -1){
-            res.push_back(nums);
-            return;
-        }
-        for(int i = pos; i <nums.size();i++){
-            swap(nums[pos],nums[i]);
-            DFS(res,nums,pos+1);
-            swap(nums[pos],nums[i]);
-        }
-    }
-    
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        
-        vector<vector<int>> res;
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <set>
+// #include <map>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <stack>
+// #include <queue>
+// using namespace std;
 
-        
-    }
-};
+// class Solution
+// {
+// public:
+//     vector<vector<int>> permute(vector<int> &nums)
+//     {
+//         vector<vector<int>> ret;
+//         vector<int> track;
+//         track(ret, nums, track);
+//     }
 
+// private:
+//     void backTrack(vector<vector<int>> &ret, vector<int> &nums, vector<int> &track)
+//     {
+//         if (nums.size() == track.size())
+//         {
+//             ret.push_back(track);
+//             return;
+//         }
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if ()
+//         }
+//     }
+// }
